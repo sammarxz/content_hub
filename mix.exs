aad194aa55d71236591a9e0dabaf77e1a6a3c144
Sam Marxz <sammarxzz@gmail.com>
2024-12-11 21:33:03 +0000

    chore: deploy config

--- mix.exs
+++ mix.exs
@@ -9,7 +9,13 @@ defmodule ContentHub.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        content_hub: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
